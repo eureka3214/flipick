bfba11a51ee5b1b951ada8b32d0c71a9c7b75f35
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 15:34:42 +0000

    sss

--- pages/annotate.py
+++ pages/annotate.py
@@ -1,31 +1,24 @@
-import streamlit as st
 import fitz
+import streamlit as st
 
-# Define function to extract text from PDF document using PyMuPDF
-def extract_text_from_pdf(pdf_file):
-    with fitz.open(pdf_file) as doc:
-        text = ""
-        for page in doc:
-            text += page.getText()
-    return text
+uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 
-# Create Streamlit app
+if uploaded_pdf is not None:
+    doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
+   
+    text_blocks = []
+    for page in doc:
+        txtpg = page.get_textpage()
+        blocks = txtpg.extractBLOCKS()
+        text_blocks.append(blocks)
+        
+    doc.close()
 
-    # Set page title
-st.set_page_config(page_title="PDF Text Extractor")
-
-# Set page layout
-st.title("PDF Text Extractor")
-st.write("Upload a PDF file to extract all text contents from the document.")
-
-# Allow user to upload a file
-uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-
-# If file is uploaded
-if uploaded_file is not None:
-    # Extract text from PDF document
-    text = extract_text_from_pdf(uploaded_file)
-
-    # Display text in an expandable widget based on layout hierarchy
-    with st.expander("Layout Hierarchy"):
-        st.write(text)
+    for page_num, blocks in enumerate(text_blocks):
+        st.write(f"Page {page_num + 1}")
+        for block_num, block in enumerate(blocks):
+            container = st.container()
+            container.radio(label=block["lines"][0]["spans"][0]["text"], options=[block_num])
+            if container.radio_selected:
+                bbox = block["bbox"]
+                st.write(f"Bbox coordinates: {bbox}")
