dfa550011cbe7686a4237418dc0ec28b909aed64
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 16:15:50 +0000

    ddd

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 if uploaded_pdf is not None:
     doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
     toc = doc.get_toc()
-    st.write(toc) 
+    for t in toc:
+        st.write(t)
+    # st.write(toc) 
     for page in doc:
         txtpg = page.get_textpage()
         
