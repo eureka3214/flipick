742c1f2be66bbda38d9c3ca0a5bd26617dd0e303
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 13:19:58 +0000

    ddd

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 
 if uploaded_pdf is not None:
     doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
-    text = ""
-    for page in doc:
-        text += page.getText()
-    st.write(text) 
+    cat = doc.pdf_catalog()
+    st.write(doc.xref_object(cat))
+    # text = ""
+    # for page in doc:
+    #     text += page.getText()
+    # st.write(text) 
     doc.close()
