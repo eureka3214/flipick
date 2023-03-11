eb7a271e508b8a991b0a919a9e312c8a269b70d7
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 20:01:41 +0000

    sss

--- pages/search_fields.py
+++ pages/search_fields.py
@@ -7,14 +7,14 @@ import fitz
 
 if __name__ == '__main__':
     def find_text_in_pdf(pdf_file, search_text):
-    doc = fitz.open(pdf_file)
-    blocks = []
-    for page in doc:
-        block = page.search_for(search_text)
-        if block:
-            blocks.append((page.number, block))
-    return blocks
-    
+        doc = fitz.open(pdf_file)
+        blocks = []
+        for page in doc:
+            block = page.search_for(search_text)
+            if block:
+                blocks.append((page.number, block))
+        return blocks
+
     st.title('PDF Text Search')
     pdf_file = st.file_uploader('Upload PDF', type=['pdf'])
     if pdf_file is not None:
