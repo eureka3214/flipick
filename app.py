25e927167556ce9e3b5f27ecf7efff849591755c
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 14:03:54 +0000

    sss

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if uploaded_pdf is not None:
     text = ""
     for page in doc:
         txtpg = page.get_textpage()
-        pix = page.get_xobjects()
+        pix = page.get_toc()
         # st.image(pix)
         st.write(pix) 
         
