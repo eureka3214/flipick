9a1dd3078e7aece3ab79d729e0a0d62229336d76
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 16:56:04 +0000

    ddd

--- pages/extract_blocks.py
+++ pages/extract_blocks.py
@@ -18,7 +18,7 @@ if uploaded_pdf is not None:
         blocks = txtpg.extractBLOCKS()
         for block in blocks:
             container = st.expander(str(block))
-            container.checkbox("select to group", label_visibility="hidden", key=str(block[0][0]))
+            container.checkbox("select to group", label_visibility="hidden", key=str(block[0][0][0]))
             with container:
                
                 st.write(block)
