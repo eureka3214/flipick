5d17dcbc513335b4651470b4cf7a5927999e8f77
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 13:15:01 +0000

    kk

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import streamlit as st
 uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 
 if uploaded_pdf is not None:
-    with fitz.open(stream=uploaded_pdf.read(), filetype="pdf") as doc:
-        text = ""
-        for page in doc:
-            text += page.getText()
-        st.write(text)
+    doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.getText()
+    st.write(text) 
+    doc.close()
