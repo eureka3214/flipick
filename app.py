301cec4ba7d5ca668ebc497629a76747cb0cbe04
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 14:14:18 +0000

    ,,,

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ if uploaded_pdf is not None:
     doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
     # cat = doc.get_toc(simple=False)
     # st.write(doc.xref_object(cat))
-    st.write(cat)
+    # st.write(cat)
     text = ""
     for page in doc:
         txtpg = page.get_textpage()
