4e15d24b1c01e49d024f4aed0807a32c741995cf
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 20:38:45 +0000

    sss

--- pages/find_font.py
+++ pages/find_font.py
@@ -22,7 +22,7 @@ if uploaded_file is not None:
             text = block[4]
             font_name = block[5]
             font_size = block[6]
-            st.write(block)
+            st.write(text)
             # Display font name and size
             # st.write(f"{text} - Font: {font_name}, Size: {font_size}")
 
