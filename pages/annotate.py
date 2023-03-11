538f93dfeca9d59a5d9a38eb08c25a1102121ec0
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 15:18:10 +0000

    ddd

--- pages/annotate.py
+++ pages/annotate.py
@@ -10,22 +10,22 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 # Create Streamlit app
-def app():
+
     # Set page title
-    st.set_page_config(page_title="PDF Text Extractor")
+st.set_page_config(page_title="PDF Text Extractor")
 
-    # Set page layout
-    st.title("PDF Text Extractor")
-    st.write("Upload a PDF file to extract all text contents from the document.")
+# Set page layout
+st.title("PDF Text Extractor")
+st.write("Upload a PDF file to extract all text contents from the document.")
 
-    # Allow user to upload a file
-    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+# Allow user to upload a file
+uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-    # If file is uploaded
-    if uploaded_file is not None:
-        # Extract text from PDF document
-        text = extract_text_from_pdf(uploaded_file)
+# If file is uploaded
+if uploaded_file is not None:
+    # Extract text from PDF document
+    text = extract_text_from_pdf(uploaded_file)
 
-        # Display text in an expandable widget based on layout hierarchy
-        with st.expander("Layout Hierarchy"):
-            st.write(text)
+    # Display text in an expandable widget based on layout hierarchy
+    with st.expander("Layout Hierarchy"):
+        st.write(text)
