e7b45ef699f8be5c9bd0ea1ca2d006302ce35679
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 18:53:21 +0000

    nnn

--- pages/extract_blocks.py
+++ pages/extract_blocks.py
@@ -28,7 +28,7 @@ if uploaded_pdf is not None:
         blocks = txtpg.extractBLOCKS()
         for block in blocks:
             container = st.expander(str(block))
-            container.checkbox("select to group", label_visibility="hidden", key=str(block[5]))
+            container.checkbox("select to group", label_visibility="hidden", key=str(block[4]))
             with container:
                 # add a dropdown to select the variable to add the block to
                 var_selection = st.selectbox("Add to variable:", key=str(block[5]), options=list(variables_dict.keys()))
