98045d2a4cc5187b59bfb373c110e13ebaf95d5d
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 18:26:00 +0000

    ddd

--- pages/extract_blocks.py
+++ pages/extract_blocks.py
@@ -5,24 +5,35 @@ uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 
 if uploaded_pdf is not None:
     doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
-    # toc = doc.get_toc()
-    
-    # # for t in toc:
-    # #     t += " modified by set_toc"  
-    # # doc.set_toc(toc)
-    # st.write(toc)
-    # doc.close() 
+
+    # create an empty dictionary to hold the variables
+    variables_dict = {}
+
+    # add a button to create a new variable and add it to the dictionary
+    if st.button("Add"):
+        # prompt the user for a name for the variable
+        var_name = st.text_input("Enter a name for the variable:")
+        if var_name:
+            # create a new variable with the name entered by the user
+            exec(f"{var_name} = ''")
+            # add the variable to the dictionary
+            variables_dict[var_name] = []
+
     for page in doc:
         txtpg = page.get_textpage()
-        
+
         blocks = txtpg.extractBLOCKS()
         for block in blocks:
             container = st.expander(str(block))
             container.checkbox("select to group", label_visibility="hidden", key=str(block[5]))
             with container:
-               
+                # add a dropdown to select the variable to add the block to
+                var_selection = st.selectbox("Add to variable:", options=list(variables_dict.keys()))
+                # add the block to the selected variable
+                if st.button("Add block"):
+                    variables_dict[var_selection].append(block[4])
+
                 st.write(block[4])
-        # st.markdown(html, unsafe_allow_html =True) 
-        
-        
+        # st.markdown(html, unsafe_allow_html =True)
+
     doc.close()
