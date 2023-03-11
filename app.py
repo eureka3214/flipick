aa035a2bf67a9ec90b7dc9a8c3a530a7a85eaa0f
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 14:14:00 +0000

    sss

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 
 if uploaded_pdf is not None:
     doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
-    cat = doc.get_toc(simple=False)
+    # cat = doc.get_toc(simple=False)
     # st.write(doc.xref_object(cat))
     st.write(cat)
     text = ""
     for page in doc:
         txtpg = page.get_textpage()
-        pix = page.get_xobjects()
+        pix = page.get_toc(simple=False)
         # st.image(pix)
-        # st.write(pix) 
+        st.write(pix) 
         
         # st.write(txtpg.extractDICT()) 
     # doc.close()
