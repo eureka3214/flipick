f96d53b696c9c92d71de2b152cacf2b7e869b652
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 16:06:12 +0000

    sss

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 
 if uploaded_pdf is not None:
     doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
-   
-    text = ""
+    labels = doc.get_page_labels()
+    st.write(labels) 
     for page in doc:
         txtpg = page.get_textpage()
-        labels = page.get_page_labels()
+        
         # pix = page.get_pixmap()
         # st.write(pix) 
         
-        st.write(labels) 
+        
     doc.close()
