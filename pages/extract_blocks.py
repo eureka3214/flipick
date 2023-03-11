16a55119570b069e766b94c52cff6e9231d3607b
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 19:15:32 +0000

    sss

--- pages/extract_blocks.py
+++ pages/extract_blocks.py
@@ -12,9 +12,8 @@ if uploaded_pdf is not None:
     # # doc.set_toc(toc)
     # st.write(toc)
     # doc.close() 
-    radio_pg_key = 0
-    radio_header_key = 0
-    radio_Subheader_key = 0
+    radio_title_key = 0
+  
 
     for page in doc:
         txtpg = page.get_textpage()
@@ -22,14 +21,9 @@ if uploaded_pdf is not None:
         blocks = txtpg.extractBLOCKS()
         for block in blocks:
             container = st.expander(str(block[4]))
-            radio_pg_key += 1
-            radio_header_key += 1
-            radio_Subheader_key += 1
-
-            container.radio("Paragraph", label_visibility="visible", key=radio_pg_key)
-            container.radio("Header", label_visibility="visible", key=radio_Subheader_key)
-            container.radio("subheader", label_visibility="visible", key=radio_Subheader_key)
-
+            radio_title_key += 1
+            container.radio("Content Option", options=["header", "subheader","paragraph"]label_visibility="visible", key=radio_title_key)
+            
             with container:
                
                 st.write(block)
