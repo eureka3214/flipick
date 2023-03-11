4863ff903bb1588cabe4153a81b8adc153aa2fe7
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 16:19:01 +0000

    sss

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
 if uploaded_pdf is not None:
     doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
     toc = doc.get_toc()
+    
     for t in toc:
-        st.write(t)
-    # st.write(toc) 
+        toc[1][1] += " modified by set_toc"  
+    doc.set_toc(toc)
+    st.write(toc) 
     for page in doc:
         txtpg = page.get_textpage()
         
