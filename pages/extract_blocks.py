f285bf122fbb6d36742bfa04952b196324a5c920
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 16:41:15 +0000

    dddd

---
+++ pages/extract_blocks.py
@@ -0,0 +1,26 @@
+import fitz
+import streamlit as st
+
+uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
+
+if uploaded_pdf is not None:
+    doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
+    # toc = doc.get_toc()
+    
+    # # for t in toc:
+    # #     t += " modified by set_toc"  
+    # # doc.set_toc(toc)
+    # st.write(toc)
+    # doc.close() 
+    for page in doc:
+        txtpg = page.get_textpage()
+        
+        blocks = txtpg.extractBLOCKS()
+        for block in blocks:
+            container = st.expander('block')
+            with container:
+                st.write(block)
+        # st.markdown(html, unsafe_allow_html =True) 
+        
+        
+    doc.close()
