1c911fa9dc147b57f304c96c3dac3515bd5d5071
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-11 20:42:18 +0000

    jnnn

--- pages/find_font.py
+++ pages/find_font.py
@@ -13,6 +13,9 @@ if uploaded_file is not None:
     # Display number of pages in PDF
     st.write(f"Number of pages: {pdf_doc.page_count}")
 
+    # Create empty list to store font names
+    font_names = []
+
     # Iterate over pages in PDF
     for page in pdf_doc:
         st.write(f"Page {page.number + 1}")
@@ -20,9 +23,12 @@ if uploaded_file is not None:
         # Iterate over text blocks in page
         for block in page.get_fonts("text"):
             font_name = block[3]
-            st.write(font_name)
-            # Display font name and size
-            # st.write(f"{text} - Font: {font_name}, Size: {font_size}")
+            if font_name not in font_names:
+                font_names.append(font_name)
 
     # Close PDF document
     pdf_doc.close()
+
+    # Display font names in select box
+    selected_font = st.selectbox("Select a font", font_names)
+    st.write(f"You selected: {selected_font}")
