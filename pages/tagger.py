dd766d5417217403203f75085ea16489fc636ab7
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-12 00:38:51 +0000

    sss

--- pages/tagger.py
+++ pages/tagger.py
@@ -20,7 +20,7 @@ if uploaded_file is not None:
     
     html = txtpg.extractHTML()
 
-    html_tag = "<div>{html}</div>"
+    html_tag = f"<div>{html}</div>"
     js_function = """
         <script>
             var headings = document.getElementsByTagName("h2");
