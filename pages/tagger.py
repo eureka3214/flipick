fb1551355d3ab10141c5df6cab4bea486737fb49
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-12 00:18:46 +0000

    sss

--- pages/tagger.py
+++ pages/tagger.py
@@ -10,17 +10,17 @@ def tag_paragraphs(headings, paragraphs):
 st.title("Tagger Application")
 
 # Input headings
-st.header("Enter the headings:")
+st.sidebar.header("Enter the headings:")
 headings = []
 for i in range(3): # change the number of headings as per your requirement
-    heading = st.text_input(f"Heading {i+1}")
+    heading = st.sidebar.text_input(f"Heading {i+1}")
     headings.append(heading)
 
 # Input paragraphs
-st.header("Enter the paragraphs:")
+st.sidebar.header("Enter the paragraphs:")
 paragraphs = []
 for i in range(3): # change the number of paragraphs as per your requirement
-    paragraph = st.text_area(f"Paragraph {i+1}")
+    paragraph = st.sidebar.text_area(f"Paragraph {i+1}")
     paragraphs.append(paragraph)
 
 # Tag paragraphs with headings
