13509d565dfaf4d210ac7b63eb5d6189f53bf436
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-12 00:59:33 +0000

    ddd

---
+++ pages/powerautomate.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write("""
+  <iframe src="https://make.powerautomate.com/{locale}/widgets/templates/?q={search term}&pagesize={number of templates}&destination={destination}&category={category}"></iframe>
+""")
